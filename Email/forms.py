27956dabb5ffb12df5dcb1392c5caca62fa11cac
Alazar <alazarthe70@gmail.com>
2025-09-21 01:01:33 +0300

    Add input form

--- Email/forms.py
+++ Email/forms.py
@@ -2,23 +2,23 @@ from django import forms
 
 class EmailFill (forms.Form):
 
-    FirstName = forms.CharField (max_length=20, widget=forms.TextInput(attrs={
+    FirstName = forms.CharField (label='',max_length=20, widget=forms.TextInput(attrs={
         'class' : 'form-control',
         'placeholder' : 'First Name'
     }))
 
-    LastName = forms.CharField (max_length=20, widget=forms.TextInput(attrs={
+    LastName = forms.CharField (label='',max_length=20, widget=forms.TextInput(attrs={
         'class' : 'form-control',
         'placeholder' : 'Last Name'
     }))
 
 
-    From = forms.EmailField (widget=forms.EmailInput(attrs={
+    From = forms.EmailField (label='',widget=forms.EmailInput(attrs={
         'class' : 'form-control',
         'placeholder' : 'example@gmail.com'
     }))
 
-    MainMessage = forms.CharField (widget=forms.Textarea(attrs={
+    MainMessage = forms.CharField (label='',widget=forms.Textarea(attrs={
         'class' : 'form-control',
         'placeholder' : 'Discribe What On Your Mind'
     }))
--- Email/views.py
+++ Email/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from termios import OPOST
+from django.shortcuts import render, redirect
+from .forms import EmailFill
+from django.core.mail import send_mail
 
 # Create your views here.
 
 def index (request):
 
-    return render (request, 'index.html')
+    #EmailSend
+
+    form = EmailFill()
+
+    if request.method == 'POST':
+
+        form = EmailFill(request.POST)
+
+        if form.is_valid():
+
+            send_mail (
+                form.cleaned_data['FirstName', 'LastName'],
+                form.cleaned_data[f"TO:"+'From'],
+                form.cleaned_data['MainMessage']
+            )
+
+            return redirect ('index')
+
+    context = {
+        'form' : form
+    }
+
+    return render (request, 'index.html', context=context)
